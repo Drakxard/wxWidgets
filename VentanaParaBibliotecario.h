5aae9ca50927a3f7a841402e4fee76b0aaac5815
Aie-s <asegovia338@gmail.com>
2026-02-04 16:43:28 -0300

    Lista bibliotecario y alumnos

--- VentanaParaBibliotecario.h
+++ VentanaParaBibliotecario.h
@@ -3,19 +3,20 @@
 #include "wxfb_project.h"
 #include "Funcionalidades/alumno/alumno.h"
 #include "Funcionalidades/system/system.h"
+#include "Funcionalidades/bibliotecario/bibliotecario.h"
 
 
 class VentanaParaBibliotecario : public MyFrameInicioCorrectoBibliotecario {
 	
 private:
 	System sistema;
-	vector<Alumno> vAlumno;
+	vector<Alumno> vAlumno; vector<Bibliotecario> vBibliotecario;
 protected:
 	void OnButtonClickPrestarLibro( wxCommandEvent& event )  override;
 	void OnButtonClickHistorialAlumno( wxCommandEvent& event )  override;
 	void OnRadioButton_CambiaPestana(wxCommandEvent& event);
-	void CargarLista(wxListCtrl* lista);
-	
+	void CargarListaAlumnos(wxListCtrl* lista);
+	void CargarListaBibliotecario(wxListCtrl* lista);
 public:
 	VentanaParaBibliotecario(wxWindow *parent=NULL);
 	~VentanaParaBibliotecario();
