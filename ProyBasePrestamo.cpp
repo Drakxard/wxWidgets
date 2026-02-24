55916c1ec7024c4c9bdea27269abe30bd18e4fa4
Aie-s <asegovia338@gmail.com>
2026-02-24 16:26:08 -0300

    vuelta un paso hacia atras

--- ProyBasePrestamo.cpp
+++ ProyBasePrestamo.cpp
@@ -88,7 +88,7 @@ MyDialogPrestamo::MyDialogPrestamo( wxWindow* parent, wxWindowID id, const wxStr
 	bSizerBuscarLibroAPrestar = new wxBoxSizer( wxHORIZONTAL );
 
 	wxBoxSizer* bSizer61;
-	bSizer61 = new wxBoxSizer( wxVERTICAL );
+	bSizer61 = new wxBoxSizer( wxHORIZONTAL );
 
 	wxBoxSizer* bSizer63;
 	bSizer63 = new wxBoxSizer( wxHORIZONTAL );
@@ -97,24 +97,11 @@ MyDialogPrestamo::MyDialogPrestamo( wxWindow* parent, wxWindowID id, const wxStr
 	m_staticText9->Wrap( -1 );
 	bSizer63->Add( m_staticText9, 0, wxALL|wxALIGN_CENTER_VERTICAL, 5 );
 
-	m_txtBuscarLibro = new wxTextCtrl( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer63->Add( m_txtBuscarLibro, 1, wxALL|wxALIGN_CENTER_VERTICAL, 5 );
-
 
 	bSizer61->Add( bSizer63, 0, wxEXPAND, 5 );
 
-	wxBoxSizer* bSizer17;
-	bSizer17 = new wxBoxSizer( wxHORIZONTAL );
-
-	m_staticText8 = new wxStaticText( this, wxID_ANY, wxT("Resultados Busqueda"), wxDefaultPosition, wxDefaultSize, 0 );
-	m_staticText8->Wrap( -1 );
-	bSizer17->Add( m_staticText8, 0, wxALL|wxALIGN_CENTER_VERTICAL, 5 );
-
-	m_listaResultadosLibros = new wxDataViewListCtrl( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxDV_ROW_LINES );
-	bSizer17->Add( m_listaResultadosLibros, 1, wxALL, 5 );
-
-
-	bSizer61->Add( bSizer17, 1, wxEXPAND, 5 );
+	m_bcomboBox1 = new wxBitmapComboBox( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, NULL, 0 );
+	bSizer61->Add( m_bcomboBox1, 1, wxALL, 5 );
 
 
 	bSizerBuscarLibroAPrestar->Add( bSizer61, 1, wxEXPAND, 5 );
@@ -122,8 +109,8 @@ MyDialogPrestamo::MyDialogPrestamo( wxWindow* parent, wxWindowID id, const wxStr
 	wxBoxSizer* bSizer62;
 	bSizer62 = new wxBoxSizer( wxHORIZONTAL );
 
-	m_btnConfirmar = new wxButton( this, wxID_ANY, wxT("Confirmar"), wxDefaultPosition, wxDefaultSize, 0 );
-	bSizer62->Add( m_btnConfirmar, 0, wxALL, 5 );
+	m_button15 = new wxButton( this, wxID_ANY, wxT("Confirmar"), wxDefaultPosition, wxDefaultSize, 0 );
+	bSizer62->Add( m_button15, 0, wxALL, 5 );
 
 
 	bSizerBuscarLibroAPrestar->Add( bSizer62, 0, wxEXPAND, 5 );
@@ -147,45 +134,44 @@ MyDialogPrestamo::MyDialogPrestamo( wxWindow* parent, wxWindowID id, const wxStr
 	wxBoxSizer* bSizerListaPrestamosAnteriores;
 	bSizerListaPrestamosAnteriores = new wxBoxSizer( wxVERTICAL );
 
-	m_gridHistorial = new wxGrid( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0 );
+	m_grid1 = new wxGrid( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0 );
 
 	// Grid
-	m_gridHistorial->CreateGrid( 5, 5 );
-	m_gridHistorial->EnableEditing( true );
-	m_gridHistorial->EnableGridLines( true );
-	m_gridHistorial->EnableDragGridSize( false );
-	m_gridHistorial->SetMargins( 0, 0 );
+	m_grid1->CreateGrid( 5, 5 );
+	m_grid1->EnableEditing( true );
+	m_grid1->EnableGridLines( true );
+	m_grid1->EnableDragGridSize( false );
+	m_grid1->SetMargins( 0, 0 );
 
 	// Columns
-	m_gridHistorial->SetColSize( 0, 47 );
-	m_gridHistorial->SetColSize( 1, 121 );
-	m_gridHistorial->SetColSize( 2, 129 );
-	m_gridHistorial->SetColSize( 3, 135 );
-	m_gridHistorial->SetColSize( 4, 205 );
-	m_gridHistorial->EnableDragColMove( false );
-	m_gridHistorial->EnableDragColSize( true );
-	m_gridHistorial->SetColLabelValue( 0, wxT("ID") );
-	m_gridHistorial->SetColLabelValue( 1, wxT("Nombre Libro") );
-	m_gridHistorial->SetColLabelValue( 2, wxT("Fecha Prestamo") );
-	m_gridHistorial->SetColLabelValue( 3, wxT("Fecha Devolucion") );
-	m_gridHistorial->SetColLabelValue( 4, wxT("Fecha Devuelto Alumno") );
-	m_gridHistorial->SetColLabelAlignment( wxALIGN_CENTER, wxALIGN_CENTER );
+	m_grid1->SetColSize( 0, 47 );
+	m_grid1->SetColSize( 1, 121 );
+	m_grid1->SetColSize( 2, 129 );
+	m_grid1->SetColSize( 3, 135 );
+	m_grid1->SetColSize( 4, 205 );
+	m_grid1->EnableDragColMove( false );
+	m_grid1->EnableDragColSize( true );
+	m_grid1->SetColLabelValue( 0, wxT("ID") );
+	m_grid1->SetColLabelValue( 1, wxT("Nombre Libro") );
+	m_grid1->SetColLabelValue( 2, wxT("Fecha Prestamo") );
+	m_grid1->SetColLabelValue( 3, wxT("Fecha Devolucion") );
+	m_grid1->SetColLabelValue( 4, wxT("Fecha Devuelto Alumno") );
+	m_grid1->SetColLabelAlignment( wxALIGN_CENTER, wxALIGN_CENTER );
 
 	// Rows
-	m_gridHistorial->SetRowSize( 0, 25 );
-	m_gridHistorial->SetRowSize( 1, 25 );
-	m_gridHistorial->SetRowSize( 2, 25 );
-	m_gridHistorial->SetRowSize( 3, 26 );
-	m_gridHistorial->SetRowSize( 4, 25 );
-	m_gridHistorial->EnableDragRowSize( true );
-	m_gridHistorial->SetRowLabelSize( 0 );
-	m_gridHistorial->SetRowLabelAlignment( wxALIGN_CENTER, wxALIGN_CENTER );
+	m_grid1->SetRowSize( 0, 25 );
+	m_grid1->SetRowSize( 1, 25 );
+	m_grid1->SetRowSize( 2, 25 );
+	m_grid1->SetRowSize( 3, 26 );
+	m_grid1->SetRowSize( 4, 25 );
+	m_grid1->EnableDragRowSize( true );
+	m_grid1->SetRowLabelAlignment( wxALIGN_CENTER, wxALIGN_CENTER );
 
 	// Label Appearance
 
 	// Cell Defaults
-	m_gridHistorial->SetDefaultCellAlignment( wxALIGN_LEFT, wxALIGN_TOP );
-	bSizerListaPrestamosAnteriores->Add( m_gridHistorial, 1, wxALL|wxEXPAND|wxALIGN_CENTER_HORIZONTAL, 5 );
+	m_grid1->SetDefaultCellAlignment( wxALIGN_LEFT, wxALIGN_TOP );
+	bSizerListaPrestamosAnteriores->Add( m_grid1, 1, wxALL|wxEXPAND|wxALIGN_CENTER_HORIZONTAL, 5 );
 
 
 	bSizer64->Add( bSizerListaPrestamosAnteriores, 1, wxEXPAND, 5 );
@@ -198,16 +184,8 @@ MyDialogPrestamo::MyDialogPrestamo( wxWindow* parent, wxWindowID id, const wxStr
 	this->Layout();
 
 	this->Centre( wxBOTH );
-
-	// Connect Events
-	m_txtBuscarLibro->Connect( wxEVT_COMMAND_TEXT_ENTER, wxCommandEventHandler( MyDialogPrestamo::OnBuscarLibro ), NULL, this );
-	m_btnConfirmar->Connect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( MyDialogPrestamo::OnConfirmarPrestamoClick ), NULL, this );
 }
 
 MyDialogPrestamo::~MyDialogPrestamo()
 {
-	// Disconnect Events
-	m_txtBuscarLibro->Disconnect( wxEVT_COMMAND_TEXT_ENTER, wxCommandEventHandler( MyDialogPrestamo::OnBuscarLibro ), NULL, this );
-	m_btnConfirmar->Disconnect( wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler( MyDialogPrestamo::OnConfirmarPrestamoClick ), NULL, this );
-
 }
